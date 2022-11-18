21635eb6000c66e0f837f0120373bc509f333e8f
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-11-18 14:37:05 +0100

    GUI adjsutments

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,11 +31,10 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.tool_panel = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.btn_text = new System.Windows.Forms.Button();
             this.btn_magnifier = new System.Windows.Forms.Button();
@@ -43,10 +42,10 @@
             this.btn_eraser = new System.Windows.Forms.Button();
             this.btn_fill = new System.Windows.Forms.Button();
             this.btn_pencil = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picture = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tool_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -54,8 +53,7 @@
             this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.viewToolStripMenuItem,
-            this.saveToolStripMenuItem});
+            this.viewToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(1198, 25);
@@ -83,14 +81,6 @@
             this.viewToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
             this.viewToolStripMenuItem.Text = "View";
             // 
-            // saveToolStripMenuItem
-            // 
-            this.saveToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saveToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
-            this.saveToolStripMenuItem.Text = "Save";
-            // 
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
@@ -109,21 +99,21 @@
             this.saveToolStripMenuItem1.Size = new System.Drawing.Size(108, 22);
             this.saveToolStripMenuItem1.Text = "Save";
             // 
-            // panel1
+            // tool_panel
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.panel1.Controls.Add(this.btn_magnifier);
-            this.panel1.Controls.Add(this.btn_pickcolor);
-            this.panel1.Controls.Add(this.btn_text);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.btn_eraser);
-            this.panel1.Controls.Add(this.btn_fill);
-            this.panel1.Controls.Add(this.btn_pencil);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 25);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1198, 143);
-            this.panel1.TabIndex = 1;
+            this.tool_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tool_panel.Controls.Add(this.btn_magnifier);
+            this.tool_panel.Controls.Add(this.btn_pickcolor);
+            this.tool_panel.Controls.Add(this.btn_text);
+            this.tool_panel.Controls.Add(this.label1);
+            this.tool_panel.Controls.Add(this.btn_eraser);
+            this.tool_panel.Controls.Add(this.btn_fill);
+            this.tool_panel.Controls.Add(this.btn_pencil);
+            this.tool_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tool_panel.Location = new System.Drawing.Point(0, 25);
+            this.tool_panel.Name = "tool_panel";
+            this.tool_panel.Size = new System.Drawing.Size(1198, 143);
+            this.tool_panel.TabIndex = 1;
             // 
             // label1
             // 
@@ -216,14 +206,14 @@
             this.btn_pencil.TabIndex = 0;
             this.btn_pencil.UseVisualStyleBackColor = false;
             // 
-            // pictureBox1
+            // picture
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(24, 184);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1152, 664);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.picture.BackColor = System.Drawing.Color.White;
+            this.picture.Location = new System.Drawing.Point(8, 176);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(800, 600);
+            this.picture.TabIndex = 2;
+            this.picture.TabStop = false;
             // 
             // Form1
             // 
@@ -231,17 +221,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(1198, 879);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.tool_panel);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tool_panel.ResumeLayout(false);
+            this.tool_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -255,8 +245,7 @@
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel tool_panel;
         private System.Windows.Forms.Button btn_pencil;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn_eraser;
@@ -264,7 +253,7 @@
         private System.Windows.Forms.Button btn_magnifier;
         private System.Windows.Forms.Button btn_pickcolor;
         private System.Windows.Forms.Button btn_text;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picture;
     }
 }
 
